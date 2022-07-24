fab0144d7d3ff58cd9873acaea4beaf787f1fe0c
nathanoliveira23 <nathanoliveira399@gmail.com>
2022-07-23 23:04:58 -0300

    ajuste na posição do close button

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -3,8 +3,8 @@ import { X } from "phosphor-react";
 
 export function CloseButton(){
     return (
-        <Popover.Button className='top-2 right-1 absolute text-zinc-400 hover:text-zinc-100' title="Fechar formulário de feedback">
-            <X weight="bold" className= 'm-3 h-4'/>
+        <Popover.Button className='absolute top-5 right-5 text-zinc-400 hover:text-zinc-100' title="Fechar formulário de feedback">
+            <X weight="bold" className= 'w-4 h-4'/>
         </Popover.Button>
     );
 }
